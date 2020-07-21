debfca3008e4110f364f0710cec754a4405896a8
Thiago Demas <thiago.demas7@gmail.com>
2020-07-21 16:34:55 -0300

    Remove Detail

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,12 @@ import {createStackNavigator} from '@react-navigation/stack';
 const AppStack = createStackNavigator();
 
 import List from './pages/Main/index';
-import Detail from './pages/Detail/index';
 
 export default function routes() {
   return (
     <NavigationContainer>
       <AppStack.Navigator screenOptions={{headerShown: false}}>
         <AppStack.Screen name="List" component={List}/>
-        <AppStack.Screen name="Detail" component={Detail}/>
       </AppStack.Navigator>
     </NavigationContainer>
   );
