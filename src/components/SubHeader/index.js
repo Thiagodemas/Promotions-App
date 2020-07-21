ca9b3831c5dd65fffd38534f0d140f66c93e14d6
Thiago Demas <thiago.demas7@gmail.com>
2020-07-21 14:19:50 -0300

    Update components and add styles

---
+++ src/components/SubHeader/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+
+import styles from './styles';
+
+
+const SubHeader = () => (
+  <View style={styles.container}>
+    <Image style={styles.avatar}
+           source={{uri: 'https://avatars0.githubusercontent.com/u/6172978?s=460&u=429ed7767e15d8e5202b1ea7f92b585cd5e2294f&v=4'}}/>
+    <View style={styles.profileInfo}>
+      <Text style={styles.name}>Thiago Demas</Text>
+      <Text style={styles.bio}>Head de Programação na Rocketseat fazendo live de layout.</Text>
+
+      <View style={styles.buttonContainer}>
+
+      </View>
+
+      </View>
+
+    </View>
+);
+
+export default SubHeader;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import {View, Text} from "react-native";
 
 
-import styles from './styles';
 import Header from '../../components/Header';
 
 export default function Main() {
   return(
-    <View style={styles.container}>
+    <View style={{flex: 1}}>
       <Header />
     </View>
   );
