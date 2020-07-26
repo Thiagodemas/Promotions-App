5ac25dc3d647731c85ec24574d191a7d5f9d5044
Thiago Demas <thiago.demas7@gmail.com>
2020-07-26 20:03:59 -0300

    Update colors.js

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,12 +1,10 @@
 export default {
   primary: '#0472b4',
-
   darker: '#111',
   dark: '#333',
   regular: '#666',
   light: '#C0C0C0',
   lighter: '#eee',
   white: '#FFF',
-
   transparent: 'rgba(0, 0, 0, 0)',
 }
